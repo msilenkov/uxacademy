c2923a8b1904dbcb5e0a441fd8ab131160bfb576
msilenkov <me.silenkov@gmail.com>
2021-01-13 22:50:45 +0300

    Update demo6.js

--- js/demo6.js
+++ js/demo6.js
@@ -70,7 +70,7 @@ function render(a) {
 }
 
 function onResize() {
-    canvas.style.width = 1;
+    canvas.style.width = 3;
     canvas.style.height = '';
     width = canvas.offsetWidth;
     height = canvas.offsetHeight;
