51132e7c2567db5b10cca3aaca05a3a80e526868
msilenkov <me.silenkov@gmail.com>
2021-01-13 23:03:28 +0300

    Update demo6.js

--- js/demo6.js
+++ js/demo6.js
@@ -10,56 +10,29 @@ var renderer = new THREE.WebGLRenderer({
 });
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 renderer.setSize(width, height);
-renderer.setClearColor(0x000000, 0);
+renderer.setClearColor(0x00307A, 0);
 
 var scene = new THREE.Scene();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 var camera = new THREE.PerspectiveCamera(7, width / height, 20, 500);
 camera.position.set(40, 0, 280);
-=======
-var camera = new THREE.PerspectiveCamera(10, width / height, 20, 500);
-=======
-var camera = new THREE.PerspectiveCamera(40, width / height, 0.6, 1000);
->>>>>>> parent of f6cba70... Update demo6.js
-=======
-var camera = new THREE.PerspectiveCamera(200, width / height, 0.1, 1000);
->>>>>>> parent of 91d781b... Update demo6.js
-=======
-var camera = new THREE.PerspectiveCamera(200, width / height, 0.1, 1000);
->>>>>>> parent of 91d781b... Update demo6.js
-=======
-var camera = new THREE.PerspectiveCamera(200, width / height, 0.1, 1000);
->>>>>>> parent of 91d781b... Update demo6.js
-camera.position.set(0, 0, 280);
->>>>>>> parent of 868d67f... Update demo6.js
 
 var sphere = new THREE.Group();
 scene.add(sphere);
 var mat1 = new THREE.LineBasicMaterial({
-    color:0x012762
+    color:0x003990
 });
 var mat2 = new THREE.LineBasicMaterial({
     color:0x012762
 });
 
-<<<<<<< HEAD
 var radius = 200;
 var lines = 60;
 var dots = 60;
-=======
-var radius = 100;
-var lines = 50;
-var dots = 50;
->>>>>>> parent of 09c7f03... Update demo6.js
 for(var i = 0; i < lines; i++) {
     var geometry = new THREE.Geometry();
     var line = new THREE.Line(this.geometry, (Math.random()>0.2)?mat1:mat2);
-    line.speed = Math.random() * 800 + 250;
+    line.speed = Math.random() * 1200 + 250;
     line.wave = Math.random();
     line.radius = Math.floor(radius + (Math.random()-0.5) * (radius*0.2));
     for(var j=0;j<dots;j++){
@@ -97,7 +70,7 @@ function render(a) {
 }
 
 function onResize() {
-    canvas.style.width = ;
+    canvas.style.width = '';
     canvas.style.height = '';
     width = canvas.offsetWidth;
     height = canvas.offsetHeight;
