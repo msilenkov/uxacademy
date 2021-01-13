7cb09f6244682409376ef5d44676d174f1d8666e
msilenkov <me.silenkov@gmail.com>
2021-01-13 22:20:09 +0300

    Update demo6.js

--- js/demo6.js
+++ js/demo6.js
@@ -14,7 +14,7 @@ renderer.setClearColor(0x000000, 0);
 
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera(40, width / height, 2, 1000);
+var camera = new THREE.PerspectiveCamera(40, width / height, 2, 500);
 camera.position.set(0, 0, 280);
 
 var sphere = new THREE.Group();
